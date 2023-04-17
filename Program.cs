ed74db059dd9beeb7c08b1bf40a9142d38e374d0
Paulo Henrique Gonçalves de Oliveira <paulo.oliveira.phgo@gmail.com>
2023-04-17 08:19:48 -0300

    atividade concluida

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿// Escreva um algoritmo que imprima a tabuada (de 1 a 10) para os números de 1 a 10.
+
+// Exemplo: tabuada do 1, tabuada do 2, etc...
+// Dica: utilize um laço dentro do outro.
+
+
+
+
+for (int i = 1; i <= 10; i++)
+{
+Console.WriteLine(@$"
+I.....................I
+   
+    Tabuada do {i}    
+                     
+I.....................I
+");
+
+
+for (int t = 1; t <= 10; t++)
+{
+
+Console.WriteLine($"{i} X {t} = {i*t}");
+
+}
+
+
+}
